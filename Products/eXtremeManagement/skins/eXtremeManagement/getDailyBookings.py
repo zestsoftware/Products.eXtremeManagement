d8bf4b332aadfe33953a2064385f868a071d550b
Maurits van Rees <maurits@vanrees.org>
2006-02-08 13:49:45 +0000

    Added possibility to switch between current and previous month on booking overview.
    svn path=/eXtremeManagement/trunk/; revision=37469

--- Products/eXtremeManagement/skins/eXtremeManagement/getDailyBookings.py
+++ Products/eXtremeManagement/skins/eXtremeManagement/getDailyBookings.py
@@ -4,24 +4,76 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=year=None, month=None, memberid=None
+##parameters=
 ##title=Return the daily Bookings for some period of a person
 ##
 
 """
-Parameters:
-memberid: member id to get this booking list of.
-  Standard: None, which means, just the current logged in member.
-
-The period is a month.
-
 This function returns a list of tuples (date, totallyBooked).
 
 If totallyBooked is 0, nothing is returned for that date.
+
+
+REQUEST Parameters:
+memberid: member id to get this booking list of.
+  Standard: None, which means, just the current logged in member.
+
+year: year to display
+
+month: month to display
+
+previous: use previous month instead
+
+next: use next month instead
+
+I don't yet now a sane way to use year and month from a template.
+Stick to previous for the moment then.
+
 """
+
+def getNextYearMonth(year, month):
+    # Get the year and month for next month (watch out for December)
+    nextyear = year + month/12
+    nextmonth = (month % 12) + 1
+    return (nextyear, nextmonth)
+
+def getPrevYearMonth(year, month):
+    # Get the year and month for the previous month (watch out for January)
+    prevmonth = month - 1
+    prevyear = year
+    if prevmonth == 0:
+        prevyear = year - 1
+        prevmonth = 12
+    return (prevyear, prevmonth)
+
+REQUEST=context.REQUEST
+
+year = None
+month = None
+previous = None
+next = None
+memberid = None
+for attr in REQUEST.form.keys():
+    try:
+        value = int(REQUEST.form[attr])
+    except:
+        value = None
+    if attr == 'previous':
+        previous = value
+    elif attr == 'next':
+        next = value
+    elif attr == 'year':
+        year = value
+    elif attr == 'month':
+        month = value
+    elif attr == 'memberid':
+        memberid = value
+    else:
+        return None
 
 if year is None:
     year = DateTime().year()
+
 if month is None:
     month = DateTime().month()
 
@@ -30,11 +82,15 @@ if year < 1970:
 if month < 0 or month > 12:
     return None
 
-startDate = DateTime(year, month, 1)
+if previous:
+    year, month = getPrevYearMonth(year, month)
+if next:
+    year, month = getNextYearMonth(year, month)
 
-# Get the year and month for next month (watch out for December)
-nextyear = year + month/12
-nextmonth = (month % 12) + 1
+
+
+startDate = DateTime(year, month, 1)
+nextyear, nextmonth = getNextYearMonth(year, month)
 endDate = DateTime(nextyear, nextmonth, 1)
 
 # Where do we want to search?
