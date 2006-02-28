59981e71286ba4889be617b215619a6e76890c46
Maurits van Rees <maurits@vanrees.org>
2006-02-28 10:07:17 +0000

    * Added getIterations.py to get a list of iterations for certain states, making use of getFolderContents(). * Used this in project_view.pt.
    svn path=/eXtremeManagement/trunk/; revision=37569

---
+++ Products/eXtremeManagement/skins/eXtremeManagement/getIterations.py
@@ -0,0 +1,30 @@
+## Script (Python) "getIterations"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=states=None
+##title=Return the iterations, in catalog form
+##
+
+"""
+Parameters:
+states: Iterations with these states will be returned.
+"""
+
+# Where do we want to search?
+object = context
+
+brains = object.getFolderContents()
+
+#states = 'in-progress'
+list = []
+if states is None:
+    list = brains
+else:
+    for brain in brains:
+        if brain.review_state in states:
+            list.append(brain)
+
+return list
