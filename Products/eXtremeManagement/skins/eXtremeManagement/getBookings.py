8e8df6088114a73ede983c12872813f03c1a6487
Maurits van Rees <maurits@vanrees.org>
2006-03-01 15:55:42 +0000

    Detailed booking overview now also has links for current and previous month.
    svn path=/eXtremeManagement/trunk/; revision=37571

--- Products/eXtremeManagement/skins/eXtremeManagement/getBookings.py
+++ Products/eXtremeManagement/skins/eXtremeManagement/getBookings.py
@@ -14,6 +14,69 @@ memberid: member id to get this booking list of.
   Standard: None, which means, just the current logged in member.
 """
 
+def getNextYearMonth(year, month):
+    # Get the year and month for next month (watch out for December)
+    nextyear = year + month/12
+    nextmonth = (month % 12) + 1
+    return (nextyear, nextmonth)
+
+def getPrevYearMonth(year, month):
+    # Get the year and month for the previous month (watch out for January)
+    prevmonth = month - 1
+    prevyear = year
+    if prevmonth == 0:
+        prevyear = year - 1
+        prevmonth = 12
+    return (prevyear, prevmonth)
+
+REQUEST=context.REQUEST
+
+year = None
+month = None
+previous = None
+next = None
+memberid = None
+for attr in REQUEST.form.keys():
+    try:
+        value = int(REQUEST.form[attr])
+    except:
+        value = None
+    if attr == 'previous':
+        previous = value
+    elif attr == 'next':
+        next = value
+    elif attr == 'year':
+        year = value
+    elif attr == 'month':
+        month = value
+    elif attr == 'memberid':
+        memberid = value
+    else:
+        return None
+
+if year is None:
+    year = DateTime().year()
+
+if month is None:
+    month = DateTime().month()
+
+if year < 1970:
+    return None
+if month < 0 or month > 12:
+    return None
+
+if previous:
+    year, month = getPrevYearMonth(year, month)
+if next:
+    year, month = getNextYearMonth(year, month)
+
+
+
+startDate = DateTime(year, month, 1)
+nextyear, nextmonth = getNextYearMonth(year, month)
+endDate = DateTime(nextyear, nextmonth, 1) - 0.00001
+
+
 # Where do we want to search?
 object = context
 searchpath = '/'.join(object.getPhysicalPath())
@@ -24,6 +87,7 @@ if memberid is None:
 
 
 bookings = context.portal_catalog.searchResults(portal_type='Booking',
+                                                getBookingDate={ "query": [startDate, endDate], "range": "minmax"},
                                                 sort_on='getBookingDate',
                                                 Creator=memberid,
                                                 path=searchpath)
