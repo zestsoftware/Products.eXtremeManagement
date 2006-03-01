cf0e0952c075428e297d2157003f08100a11237a
Maurits van Rees <maurits@vanrees.org>
2006-03-01 14:14:43 +0000

    Stories in Iterations are now sorted with completed first, then the rest of the sorted folderContents.
    svn path=/eXtremeManagement/trunk/; revision=37570

--- Products/eXtremeManagement/skins/eXtremeManagement/getSortedStories.py
+++ Products/eXtremeManagement/skins/eXtremeManagement/getSortedStories.py
@@ -1,17 +1,23 @@
-## Script (Python) "getSortedStories"
+## Script (Python) ""
 ##bind container=container
 ##bind context=context
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
 ##parameters=
-##title=Get open tasks for assignee
+##title=Get completed items first, then rest of ordered folder contents
 ##
 
-iteration_path = '/'.join(context.getPhysicalPath())
+items = context.getFolderContents()
 
-items = context.portal_catalog.searchResults(portal_type='Story',
-                                             sort_on='review_state',
-                                             path=iteration_path)
+completed = []
+uncompleted = []
 
-return items
+for item in items:
+    if item.review_state == 'completed':
+        completed.append(item)
+    else:
+        uncompleted.append(item)
+
+
+return completed + uncompleted
