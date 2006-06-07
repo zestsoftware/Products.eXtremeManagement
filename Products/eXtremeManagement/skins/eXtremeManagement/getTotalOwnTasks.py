0eff620876a04565e5ab09f46b734acfc3d1c7d2
Maurits van Rees <maurits@vanrees.org>
2006-06-07 13:59:30 +0000

    Added total time of own tasks in several pages.  This makes it slower though. Adding some Task variables to the catalog may help.
    svn path=/eXtremeManagement/trunk/; revision=37645

---
+++ Products/eXtremeManagement/skins/eXtremeManagement/getTotalOwnTasks.py
@@ -0,0 +1,40 @@
+## Script (Python) "getTotalOwnTasks"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=states=None
+##title=Get total estimate, etc for my tasks in the current context
+##
+
+"""
+Parameters:
+states: Tasks with these states will be returned.
+
+"""
+
+required_states = states
+# showEveryonesTasks: if False, only tasks assigned to the current user
+# will be reported.
+showEveryonesTasks = False
+taskbrains = context.getTasks(required_states, showEveryonesTasks)
+tasks = [taskbrain.getObject() for taskbrain in taskbrains]
+
+# HACK: find a ProjectFolder so we can call the formatTime() function
+# from that projectfolder later.
+# Batlogg is busy putting that function somewhere else, which is good. :)
+pf = context.portal_catalog.searchResults(portal_type='ProjectFolder')
+projectFolder = pf[0].getObject()
+formatTime = projectFolder.formatTime
+
+def myPortion(task):
+    return 1.0/len(task.getAssignees())
+
+rawEstimate = sum([task.getRawEstimate() * myPortion(task)
+                   for task in tasks])
+rawActualHours = sum([task.getRawActualHours() * myPortion(task)
+                      for task in tasks])
+rawDifference = sum([task.getRawDifference() * myPortion(task)
+                     for task in tasks])
+return map(formatTime, (rawEstimate, rawActualHours, rawDifference))
