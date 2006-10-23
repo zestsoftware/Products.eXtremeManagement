c5c74cae26103c115abd1461fbe20415a1dd8a70
Maurits van Rees <maurits@vanrees.org>
2006-10-23 08:01:30 +0000

    Get overview of total estimate etc of tasks per person in the iteration view.
    svn path=/eXtremeManagement/trunk/; revision=37722

---
+++ Products/eXtremeManagement/skins/eXtremeManagement/getTotalTasksForAll.py
@@ -0,0 +1,49 @@
+## Script (Python) "getTotalOwnTasks"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=states=None
+##title=Get a list for each user of total estimate, etc for tasks in
+##the current context
+##
+
+"""
+Parameters:
+states: Tasks with these states will be returned.
+
+"""
+
+xt = context.xm_tool
+
+# Where do we want to search?
+searchpath = '/'.join(context.getPhysicalPath())
+
+if states is None:
+    taskbrains = context.portal_catalog.searchResults(portal_type='Task',
+                                                 path=searchpath)
+else:
+    taskbrains = context.portal_catalog.searchResults(portal_type='Task',
+                                                 review_state=states,
+                                                 path=searchpath)
+
+def myPortion(task):
+    return 1.0/len(task.getAssignees)
+
+list = []
+members = context.getProject().getMembers()
+for memberid in members:
+    tasks = [taskbrain for taskbrain in taskbrains
+                    if memberid in taskbrain.getAssignees]
+    rawEstimate = sum([task.getRawEstimate * myPortion(task)
+                       for task in tasks])
+    if rawEstimate > 0:
+        rawActualHours = sum([task.getRawActualHours * myPortion(task)
+                              for task in tasks])
+        rawDifference = sum([task.getRawDifference * myPortion(task)
+                             for task in tasks])
+        list.append((memberid,
+                     map(xt.formatTime, (rawEstimate, rawActualHours, rawDifference))))
+
+return list
