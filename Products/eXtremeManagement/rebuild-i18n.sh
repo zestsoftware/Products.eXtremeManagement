49bf6420b5ec0dba938b913859ce7037449e81df
Maurits van Rees <maurits@vanrees.org>
2007-02-07 12:18:09 +0000

    Fixed all errors reported by i18ndude.
    svn path=/eXtremeManagement/trunk/; revision=37766

--- Products/eXtremeManagement/rebuild-i18n.sh
+++ Products/eXtremeManagement/rebuild-i18n.sh
@@ -2,15 +2,21 @@
 
 # Project name
 PROJECT=eXtremeManagement
+
 # Find i18n attributes in these files:
-TEMPLATE_FILES=$(find -name '*pt')
+# Ignore booking_year.pt for now, as i18ndude does not like it.
+# Browser views can help there.
+# Also ignore xm_dashboard.pt as that has too many errors according to
+# i18ndude, and I do not use it currently.
+TEMPLATE_FILES=$(find -name booking_year.pt -prune -o -name xm_dashboard.pt -prune -o -name '*pt' -print)
 # Directory that will be search for i18n tags, including subdirs.
 SEARCH_DIR=skins/$PROJECT
 # Merge the contents of this file into the i18n created file.
 SELF_MADE=i18n/generated.pot
 
 # Rebuild the .pot file of our project and merge the $SELF_MADE file in it.
-i18ndude rebuild-pot --pot i18n/$PROJECT.pot --create $PROJECT --merge $SELF_MADE $SEARCH_DIR
+i18ndude rebuild-pot --pot i18n/$PROJECT.pot --create $PROJECT --merge $SELF_MADE $TEMPLATE_FILES
+# Instead of $TEMPLATE_FILES you could use $SEARCH_DIR.
 
 i18ndude sync --pot i18n/$PROJECT.pot i18n/$PROJECT-*.po
 
