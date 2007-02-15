36443fdb23a5eb92612e8a4ccd4557970ab2c55a
Maurits van Rees <maurits@vanrees.org>
2007-02-15 10:06:01 +0000

    Use getObjPositionInParent instead of own getPosition script.
    svn path=/eXtremeManagement/trunk/; revision=37882

--- Products/eXtremeManagement/skins/eXtremeManagement/getPosition.py
+++
@@ -1,9 +0,0 @@
-## Script (Python) "getStoryPosition"
-##title=Return the index of the current Story
-##parameters=obj=None,stories=None
-
-for i in range(len(stories)):
-    if stories[i].getId == obj.getId():
-        return i
-return None
-
