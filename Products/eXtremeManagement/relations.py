976635c42a9ab2d2ae32ce83b1c39a9fa104b518
Maurits van Rees <maurits@vanrees.org>
2007-02-20 11:29:51 +0000

    Removed unused relations.py
    svn path=/eXtremeManagement/trunk/; revision=38148

--- Products/eXtremeManagement/relations.py
+++
@@ -1,7 +0,0 @@
-from Products.Archetypes.ReferenceEngine import Reference, ReferenceException
-from Products.CMFCore.utils import getToolByName
-
-class CustomerProjectRelation(Reference):
-    relationship = 'CustProjRelation'
-
-
