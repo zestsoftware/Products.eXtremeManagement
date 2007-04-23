94a3f325d57f06e724947647dad641b3f88192c8
Maurits van Rees <maurits@vanrees.org>
2007-04-23 14:29:34 +0000

    Simplify getTasks.py, which we want to get rid of anyway.
    svn path=/eXtremeManagement/trunk/; revision=40997

--- Products/eXtremeManagement/skins/eXtremeManagement/getTasks.py
+++ Products/eXtremeManagement/skins/eXtremeManagement/getTasks.py
@@ -19,20 +19,12 @@ will be reported.
 # Where do we want to search?
 searchpath = '/'.join(context.getPhysicalPath())
 
-if states is None:
-    taskbrains = context.portal_catalog.searchResults(portal_type='Task',
-                                                 path=searchpath)
-else:
-    taskbrains = context.portal_catalog.searchResults(portal_type='Task',
-                                                 review_state=states,
-                                                 path=searchpath)
+filter = dict(portal_type='Task',
+              review_state=states,
+              path=searchpath)
 
-list = []
-if showEveryonesTasks:
-    list = taskbrains
-else:
+if not showEveryonesTasks:
     member = context.portal_membership.getAuthenticatedMember()
-    memberid = member.id
-    list = [taskbrain for taskbrain in taskbrains
-            if memberid in taskbrain.getAssignees]
-return list
+    filter['getAssignees'] = member.id
+
+return context.portal_catalog.searchResults(**filter)
