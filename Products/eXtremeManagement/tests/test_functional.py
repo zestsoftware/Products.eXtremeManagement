ef05f2f4b330a6a4f9e096f5b329f9ea3e8988a7
Daniel Nouri <daniel.nouri@gmail.com>
2007-05-11 19:26:06 +0000

    REPORT_ONLY_FIRST_FAILURE is best handled with the -1 argument to the testrunner.
    svn path=/eXtremeManagement/trunk/; revision=41999

--- Products/eXtremeManagement/tests/test_functional.py
+++ Products/eXtremeManagement/tests/test_functional.py
@@ -23,8 +23,7 @@ REQUIRE_TESTBROWSER = [
     'create.txt',
     ]
 
-OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
-               doctest.ELLIPSIS |
+OPTIONFLAGS = (doctest.ELLIPSIS |
                doctest.NORMALIZE_WHITESPACE)
 
 def list_doctests():
