6d553b4dca3e50231842bbb7f58b529185f96ea4
Maurits van Rees <maurits@vanrees.org>
2007-06-07 17:23:54 +0000

    - Get rid of lines in ProjectFolder class that are also in GS. - Add aliases so this works on plone 3 too (and keeps working on 2.5)
    svn path=/eXtremeManagement/trunk/; revision=43353

--- Products/eXtremeManagement/content/ProjectFolder.py
+++ Products/eXtremeManagement/content/ProjectFolder.py
@@ -23,28 +23,8 @@ class ProjectFolder(OrderedBaseFolder):
     # This name appears in the 'add' box
     archetype_name = 'Project Folder'
     portal_type = meta_type = 'ProjectFolder'
-    allowed_content_types = ['Project']
-    filter_content_types = 1
-    global_allow = 1
-    content_icon = 'project_icon.gif'
-    immediate_view = 'base_view'
-    default_view = 'base_view'
-    suppl_views = ()
-    typeDescription = "ProjectFolder"
-    typeDescMsgId = 'description_edit_projectfolder'
     _at_rename_after_creation = True
     schema = ProjectFolder_schema
 
-    actions =  ({'action': "string:${object_url}/project_listing",
-                 'category': "object",
-                 'id': 'view',
-                 'name': 'view',
-                 'permissions': ("View",),
-                 'condition': 'python:1'
-                },
-                {'id': 'local_roles',
-                 'name': 'Sharing',
-                 'action': 'string:${object_url}/folder_localrole_form',
-                 'permissions': (ManageProperties,)},)
 
 registerType(ProjectFolder, PROJECTNAME)
