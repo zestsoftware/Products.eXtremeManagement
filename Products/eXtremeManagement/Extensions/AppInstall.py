9b112aac08f361ac9db18d0a488737adb42840c8
Maurits van Rees <maurits@vanrees.org>
2007-10-16 10:21:24 +0000

    Get rid of all mentioning of left_slots and right_slots properties as that is done with plone.left/RightColumn portletmanagers now.
    svn path=/eXtremeManagement/trunk/; revision=51825

--- Products/eXtremeManagement/Extensions/AppInstall.py
+++ Products/eXtremeManagement/Extensions/AppInstall.py
@@ -13,30 +13,8 @@ def applyGenericSetupProfile(portal, out):
     print >> out, "Applied the generic setup profile for eXtremeManagement"
 
 
-def removeOldTool(portal, out):
-    if 'xm_tool' in portal.objectIds():
-        portal.manage_delObjects('xm_tool')
-        print >> out, "Removed outdated xm_tool from portal."
-
-
-def uninstall(portal):
-    """Custom uninstall method for eXtremeManagement."""
-    out = StringIO()
-    removeOldTool(portal, out)
-
-    left_slots = portal.getProperty('left_slots', None)
-    remainingSlots = [slot for slot in left_slots if slot not in XM_LEFT_SLOTS]
-    portal._updateProperty('left_slots', tuple(remainingSlots)) 
-
-    right_slots = portal.getProperty('right_slots', None)
-    remainingSlots = [slot for slot in right_slots if slot not in XM_RIGHT_SLOTS]
-    portal._updateProperty('right_slots', tuple(remainingSlots)) 
-    return out.getvalue()
-
-
 def install(self):
     out = StringIO()
-    removeOldTool(self, out)
     print >> out, "Apply the generic setup profile"
     applyGenericSetupProfile(self, out)
     out.write("Successfully installed %s.\n" % PROJECTNAME)
