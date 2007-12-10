b442cf44543d983ccb5119813850a52422892f58
Reinout van Rees <reinout@vanrees.org>
2007-12-10 16:46:39 +0000

    Added cache handling
    svn path=/eXtremeManagement/trunk/; revision=55213

---
+++ Products/eXtremeManagement/browser/cache.py
@@ -0,0 +1,51 @@
+from Acquisition import aq_inner
+from Products.Five import BrowserView
+from Products.CMFCore.utils import getToolByName
+
+
+class CacheHelperView(BrowserView):
+    """Provide some helper methods for caching.
+
+    A browser view makes sure we don't do this too many times without
+    need.
+    """
+
+    def copy_happening(self):
+        """Return whether a copy is happening inside a folder.
+        """
+
+        request = self.request
+        copying = request.get('__cp', False)
+        if copying:
+            return 'copying'
+        else:
+            return 'not_copying'
+
+    def my_hours(self):
+        """Return my # of hours."""
+        context = aq_inner(self.context)
+        portal = getToolByName(context, 'portal_url').getPortalObject()
+        return portal.restrictedTraverse('@@daytotal/total')
+
+    def my_projects(self):
+        """Return hash of my projects."""
+        context = aq_inner(self.context)
+        portal = getToolByName(context, 'portal_url').getPortalObject()
+        projects = portal.restrictedTraverse('@@myprojects/projectlist')
+        uids = [project.UID for project in projects]
+        uid_string = ' '.join(uids)
+        return hash(uid_string)
+
+    def etag(self):
+        """Return normal extra ETag components for philips.
+
+        Only location is used as an extra differentiator now, but
+        later the current display and current default page should be
+        added.
+        """
+
+        params = [self.copy_happening(),
+                  self.my_hours(),
+                  self.my_projects(),
+                  ]
+        return ';'.join(params)
