8bd9c7edcd1aea5fa59f5acf4dd624349f2f3187
Maurits van Rees <maurits@vanrees.org>
2007-12-10 17:53:36 +0000

    pep8
    svn path=/eXtremeManagement/trunk/; revision=55227

--- Products/eXtremeManagement/browser/iterations.py
+++ Products/eXtremeManagement/browser/iterations.py
@@ -9,8 +9,8 @@ from Products.CMFCore.utils import getToolByName
 from Products.eXtremeManagement.browser.xmbase import XMBaseView
 from xm.booking.timing.interfaces import IActualHours
 from xm.booking.timing.interfaces import IEstimate
-from Products.eXtremeManagement.content.Iteration import UNACCEPTABLE_STATUSES \
-     as UNACCEPTABLE_STORY_STATUSES
+from Products.eXtremeManagement.content.Iteration import \
+    UNACCEPTABLE_STATUSES as UNACCEPTABLE_STORY_STATUSES
 from Products.eXtremeManagement.utils import formatTime
 from Products.eXtremeManagement.utils import getStateSortedContents
 
@@ -21,8 +21,10 @@ def _store_on_context(obj, *args, **kwargs):
         setattr(obj.context, KEY, {})
     return getattr(obj.context, KEY)
 
+
 def _render_details_cachekey(obj, storybrain):
     key = StringIO()
+
     def add(brain):
         key.write(brain.getPath())
         key.write('\n')
@@ -36,8 +38,11 @@ def _render_details_cachekey(obj, storybrain):
 
     return key.getvalue()
 
+
 def cache(get_key, get_cache):
+
     def decorator(fun):
+
         def replacement(*args, **kwargs):
             key = get_key(*args, **kwargs)
             cache = get_cache(*args, **kwargs)
@@ -47,6 +52,7 @@ def cache(get_key, get_cache):
             return cache[key]
         return replacement
     return decorator
+
 
 def degrade_headers(html, howmuch=2):
     """
@@ -60,6 +66,7 @@ def degrade_headers(html, howmuch=2):
         html = html.replace('</h%s>' % number, '</h%s>' % (number + howmuch))
     return html
 
+
 class IterationView(XMBaseView):
     """Simply return info about a Iteration.
     """
@@ -70,7 +77,6 @@ class IterationView(XMBaseView):
     <h0 tal:content='context/Title'>Story title</h0>
     <metal:use use-macro='context/story_view/macros/details' />
     """)
-    
 
     def main(self):
         """Get a dict with info from this Context.
@@ -96,12 +102,13 @@ class IterationView(XMBaseView):
         size_estimate = sum([item.size_estimate for item in items
                              if item.size_estimate is not None])
 
+        ploneview = context.restrictedTraverse('@@plone')
         returnvalue = dict(
             title = context.Title(),
             description = context.Description(),
             man_hours = context.getManHours(),
-            start_date = context.restrictedTraverse('@@plone').toLocalizedTime(context.getStartDate()),
-            end_date = context.restrictedTraverse('@@plone').toLocalizedTime(context.getEndDate()),
+            start_date = ploneview.toLocalizedTime(context.getStartDate()),
+            end_date = ploneview.toLocalizedTime(context.getEndDate()),
             estimate = formatTime(estimate),
             size_estimate = size_estimate,
             actual = formatTime(actual),
@@ -147,12 +154,12 @@ class IterationView(XMBaseView):
         searchpath = brain.getPath()
         filter = dict(portal_type=['Task', 'PoiTask'],
                       path=searchpath)
-        unfinished_states = ('open','to-do',)
+        unfinished_states = ('open', 'to-do', )
         filter['review_state'] = unfinished_states
         open_tasks = len(catalog.searchResults(**filter))
 
         # compute completed task count
-        finished_states = ('completed',)
+        finished_states = ('completed', )
         filter['review_state'] = finished_states
         completed_tasks = len(catalog.searchResults(**filter))
 
@@ -209,7 +216,6 @@ class IterationView(XMBaseView):
                    if x.review_state in UNACCEPTABLE_STORY_STATUSES]
         return ', '.join(stories)
 
-
     def get_progress_perc(self, part, total):
         """Get progress percentage of part compared to total.
 
@@ -263,13 +269,15 @@ class IterationView(XMBaseView):
                 percentage = int(round(part/float(total)*100))
             except TypeError:
                 return '??'
-            portal_properties = getToolByName(context, 'portal_properties', None)
+            portal_properties = getToolByName(context, 'portal_properties',
+                                              None)
             if portal_properties is None:
                 return percentage
             xm_props = portal_properties.get('xm_properties', None)
             if xm_props is None:
                 return percentage
-            max_percentage = xm_props.get('maximum_not_completed_percentage', 90.0)
+            max_percentage = xm_props.get('maximum_not_completed_percentage',
+                                          90.0)
             if percentage > max_percentage:
                 return max_percentage
             return percentage
