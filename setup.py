e1a83b174c9ec3c1f0557b04abc09784c560b081
Maurits van Rees <maurits@vanrees.org>
2008-01-17 18:08:59 +0000

    Add setup.py and Products dir
    svn path=/Products.eXtremeManagement/trunk/; revision=57093

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+versionfile = open(os.path.join('Products', 'eXtremeManagement', 'version.txt'))
+version = versionfile.read().strip()
+versionfile.close()
+
+setup(name='Products.eXtremeManagement',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Zest Software',
+      author_email='xp@zestsoftware.nl',
+      url='http://plone.org/products/extreme-management-tool',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'xm.booking',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
