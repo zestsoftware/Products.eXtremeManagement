65ea0c1f53cecfdbd4a4d1167661e09c809d0408
markvl <markvl@localhost>
2008-01-29 12:27:49 +0000

    Make the expanded story details (in the iteration and project view) a bit more user friendly.
    svn path=/Products.eXtremeManagement/branches/mvr-providers/; revision=57861

--- Products/eXtremeManagement/skins/eXtremeManagement/eXtreme.js
+++ Products/eXtremeManagement/skins/eXtremeManagement/eXtreme.js
@@ -7,9 +7,19 @@ function toggle_display(id) {
     } else {
         element.style.display = 'none';
     }
-    
+}
+
+function toggle_border(number) {
+    var header = document.getElementById('toggle-header-' + number);
+    var item = document.getElementById('toggle-item-' + number);
+    if (item.style.display == 'none') {
+	header.cells[0].className = header.cells[0].className.replace(/ clear-bottom/, '');
+    } else {
+	header.cells[0].className = header.cells[0].className + ' clear-bottom';
+    }
 }
 
 function toggle_item(number) {
     toggle_display('toggle-item-' + number);
+    toggle_border(number);
 }
