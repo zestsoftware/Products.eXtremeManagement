ebee93dd248ea28ad794100259c99b3922a8c20c
Maurits van Rees <maurits@vanrees.org>
2008-02-25 18:02:53 +0000

    Add tests for the projects portlet; tests moved and adapted from xm.portlets.
    svn path=/Products.eXtremeManagement/trunk/; revision=59416

---
+++ Products/eXtremeManagement/tests/test_projects_portlet.py
@@ -0,0 +1,84 @@
+from zope.component import getUtility, getMultiAdapter
+
+from plone.portlets.interfaces import IPortletType
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignment
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.portlets.interfaces import IPortletRenderer
+
+from plone.app.portlets.storage import PortletAssignmentMapping
+
+from xm.portlets import project
+
+from Products.eXtremeManagement.tests.base import eXtremeManagementTestCase
+
+
+class TestPortlet(eXtremeManagementTestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager',))
+
+    def test_portlet_type_registered(self):
+        portlet = getUtility(IPortletType, name='xm.portlets.project')
+        self.assertEquals(portlet.addview, 'xm.portlets.project')
+
+    def test_interfaces(self):
+        # TODO: Pass any keyword arguments to the Assignment constructor
+        portlet = project.Assignment()
+        self.failUnless(IPortletAssignment.providedBy(portlet))
+        self.failUnless(IPortletDataProvider.providedBy(portlet.data))
+
+    def test_invoke_add_view(self):
+        portlet = getUtility(IPortletType, name='xm.portlets.project')
+        mapping = self.portal.restrictedTraverse('++contextportlets++plone.leftcolumn')
+        for m in mapping.keys():
+            del mapping[m]
+        addview = mapping.restrictedTraverse('+/' + portlet.addview)
+
+        addview()
+
+        self.assertEquals(len(mapping), 1)
+        self.failUnless(isinstance(mapping.values()[0], project.Assignment))
+
+    def test_obtain_renderer(self):
+        context = self.folder
+        request = self.folder.REQUEST
+        view = self.folder.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager, name='plone.rightcolumn', context=self.portal)
+
+        # TODO: Pass any keyword arguments to the Assignment constructor
+        assignment = project.Assignment()
+
+        renderer = getMultiAdapter((context, request, view, manager, assignment), IPortletRenderer)
+        self.failUnless(isinstance(renderer, project.Renderer))
+
+
+class TestRenderer(eXtremeManagementTestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager',))
+
+    def renderer(self, context=None, request=None, view=None, manager=None, assignment=None):
+        context = context or self.folder
+        request = request or self.folder.REQUEST
+        view = view or self.folder.restrictedTraverse('@@plone')
+        manager = manager or getUtility(IPortletManager, name='plone.rightcolumn', context=self.portal)
+
+        # TODO: Pass any default keyword arguments to the Assignment constructor
+        assignment = assignment or project.Assignment()
+        return getMultiAdapter((context, request, view, manager, assignment), IPortletRenderer)
+
+    def test_render(self):
+        # TODO: Pass any keyword arguments to the Assignment constructor
+        r = self.renderer(context=self.portal, assignment=project.Assignment())
+        r = r.__of__(self.folder)
+        r.update()
+        output = r.render()
+        # TODO: Test output
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestPortlet))
+    suite.addTest(makeSuite(TestRenderer))
+    return suite
