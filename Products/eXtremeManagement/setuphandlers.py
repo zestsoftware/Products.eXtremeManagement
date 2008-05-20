b110a97be05ab4d74248a555658533c8debbdd0f
Maurits van Rees <maurits@vanrees.org>
2008-05-20 11:53:55 +0000

    Remove the unused migration functions from setuphandlers.py.
    svn path=/Products.eXtremeManagement/trunk/; revision=65232

--- Products/eXtremeManagement/setuphandlers.py
+++ Products/eXtremeManagement/setuphandlers.py
@@ -1,6 +1,7 @@
 from Products.CMFCore.utils import getToolByName
 from Products.eXtremeManagement import config
 
+
 def addCatalogIndexes(site, logger):
     """Add our indexes to the catalog.
 
@@ -16,54 +17,6 @@ def addCatalogIndexes(site, logger):
         if name not in indexes:
             catalog.addIndex(name, meta_type)
             logger.info("Added %s for field %s.", meta_type, name)
-
-
-def _migrateSchema(site, contentType, logger):
-    at = getToolByName(site, 'archetype_tool')
-    class dummy:
-        form = {}
-    dummyRequest = dummy()
-    dummy.form[contentType] = 1
-    dummy.form['update_all'] = 1
-    at.manage_updateSchema(update_all=1,
-                           REQUEST=dummyRequest)
-    logger.info('Migrated schema of %s', contentType)
-
-def _migrate_project(portal, logger):
-    _migrateSchema(portal, 'eXtremeManagement.Project', logger)
-
-def _migrate_iterations(portal, logger):
-    _migrateSchema(portal, 'eXtremeManagement.Iteration', logger)
-
-def migrate_stories(portal, logger):
-    _migrateSchema(portal, 'eXtremeManagement.Story', logger)
-
-def migrate_tasks(portal, logger):
-    """
-    Add a property to the portal so that other parts now that there is
-    a schema update going on for the Tasks.  Main reason: if this is
-    True, then _do not_ send an email for every Task that is getting
-    assigned.  See Task.setAssigned()
-    """
-    portal_properties = getToolByName(portal, 'portal_properties')
-    xm_props = portal_properties.xm_properties
-    ori = xm_props.send_task_mails
-    xm_props.send_task_mails = False
-    _migrateSchema(portal, 'eXtremeManagement.Task', logger)
-    _migrateSchema(portal, 'eXtremeManagement.PoiTask', logger)
-    xm_props.send_task_mails = ori
-
-def migrate_bookings(portal, logger):
-    _migrateSchema(portal, 'eXtremeManagement.Booking', logger)
-
-def migrate_ct(portal, logger):
-    #migrate_projects(portal, logger)
-    #migrate_iterations(portal, logger)
-    #migrate_stories(portal, logger)
-    #migrate_tasks(portal, logger)
-    #migrate_bookings(portal, logger)
-    # Nothing needed at the moment.
-    pass
 
 
 def configureKupu(portal, logger):
@@ -89,12 +42,13 @@ def configureKupu(portal, logger):
 
     # kupu_library_tool has an idiotic interface, basically written purely to
     # work with its configuration page. :-(
-    kupuTool.updateResourceTypes(({'resource_type' : 'linkable',
-                                   'old_type'      : 'linkable',
-                                   'portal_types'  :  linkable},
-                                  {'resource_type' : 'collection',
-                                   'old_type'      : 'collection',
-                                   'portal_types'  :  collection},))
+    kupuTool.updateResourceTypes(({'resource_type': 'linkable',
+                                   'old_type': 'linkable',
+                                   'portal_types': linkable},
+                                  {'resource_type': 'collection',
+                                   'old_type': 'collection',
+                                   'portal_types': collection},
+                                  ))
     logger.info('Added our types to collection and linkable of kupu')
 
 
@@ -115,7 +69,6 @@ def importVarious(context):
     site = context.getSite()
     # Integrate our types in kupu, if it is installed.
     configureKupu(site, logger)
-    migrate_ct(site, logger)
     add_roles_that_should_be_handled_by_rolemap_xml(site, logger)
     addCatalogIndexes(site, logger)
     logger.info('eXtremeManagement_various step imported')
