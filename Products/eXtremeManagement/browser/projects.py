9c863c3020cf62c8113d1845e7cc87a6379338bf
Reinout van Rees <reinout@vanrees.org>
2008-07-04 15:49:37 +0000

    pep8
    svn path=/Products.eXtremeManagement/trunk/; revision=67870

--- Products/eXtremeManagement/browser/projects.py
+++ Products/eXtremeManagement/browser/projects.py
@@ -1,10 +1,11 @@
-from zope.component import getMultiAdapter
+from Acquisition import aq_inner
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
+from plone.memoize.view import memoize
+from zope.component import getMultiAdapter
+
 from Products.eXtremeManagement.browser.xmbase import XMBaseView
 from Products.eXtremeManagement.utils import formatTime
-from Acquisition import aq_inner
-from plone.memoize.view import memoize
 
 
 class MyProjects(BrowserView):
