0f8cc4ea5eaee21419f4cd14eb14881b61aae1aa
Maurits van Rees <maurits@vanrees.org>
2008-08-14 16:39:19 +0000

    Added Products.Poi to the install_requires in setup.py as Poi now has eggs for both branch 1.1 and trunk.
    svn path=/Products.eXtremeManagement/branches/1.6/; revision=69944

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(name='Products.eXtremeManagement',
           'xm.portlets >= 0.5, <= 0.6dev',
 	  'kss.plugin.yuidnd',
           'Products.contentmigration == 1.0b4',
+          'Products.Poi'
       ],
       entry_points="""
       # -*- Entry points: -*-
