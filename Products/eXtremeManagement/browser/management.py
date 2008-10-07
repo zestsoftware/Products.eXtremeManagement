0180891dd45b6301196a1442f38c98d1988d17df
Reinout van Rees <reinout@vanrees.org>
2008-10-07 13:29:25 +0000

    Adding /invoicing view for managers that lists all invoicable   iterations
    svn path=/Products.eXtremeManagement/trunk/; revision=73098

---
+++ Products/eXtremeManagement/browser/management.py
@@ -0,0 +1,80 @@
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from zope.cachedescriptors.property import Lazy
+from plone.memoize.view import memoize
+from zope.component import getMultiAdapter
+try:
+    import xm.theme
+except ImportError:
+    HAS_XM_THEME = False
+else:
+    HAS_XM_THEME = True
+
+
+from Products.eXtremeManagement.browser.xmbase import XMBaseView
+from Products.eXtremeManagement.utils import formatTime
+
+
+class InvoicingView(XMBaseView):
+
+    def projectlist(self):
+        context = aq_inner(self.context)
+        searchpath = '/'.join(context.getPhysicalPath())
+        # Get a list of all projects
+        projectbrains = self.catalog.searchResults(portal_type='Project',
+                                                   path=searchpath)
+
+        for projectbrain in projectbrains:
+            searchpath = projectbrain.getPath()
+            # Search for Iterations that are ready to get invoiced
+            iterationbrains = self.catalog.searchResults(
+                portal_type='Iteration', review_state='completed',
+                path=searchpath)
+            if len(iterationbrains) > 0:
+                iteration_list = []
+                for iterationbrain in iterationbrains:
+                    info = self.iterationbrain2dict(iterationbrain)
+                    iteration_list.append(info)
+                info = self.projectbrain2dict(projectbrain)
+                info['iterations'] = iteration_list
+                yield info
+
+    def iterationbrain2dict(self, brain):
+        """Get a dict with info from this iteration brain.
+        """
+        review_state_id = brain.review_state
+        estimate = brain.estimate
+        actual = brain.actual_time
+        obj = brain.getObject()
+        history = self.workflow.getHistoryOf('eXtreme_Iteration_Workflow',
+                                             obj)
+        completion_date = None
+        for item in history:
+            if item['action'] == 'complete':
+                completion_date = item['time']
+        returnvalue = dict(
+            url = brain.getURL(),
+            title = brain.Title,
+            description = brain.Description,
+            icon = brain.getIcon,
+            man_hours = brain.getManHours,
+            estimate = formatTime(estimate),
+            actual = formatTime(actual),
+            difference = formatTime(estimate - actual),
+            review_state = review_state_id,
+            review_state_title = self.workflow.getTitleForStateOnType(
+                                 review_state_id, 'Iteration'),
+            completion_date = completion_date,
+            brain= brain,
+        )
+        return returnvalue
+
+    def projectbrain2dict(self, brain):
+        """Get a dict with info from this project brain.
+        """
+        returnvalue = dict(
+            url = brain.getURL(),
+            title = brain.Title,
+            description = brain.Description,
+        )
+        return returnvalue
