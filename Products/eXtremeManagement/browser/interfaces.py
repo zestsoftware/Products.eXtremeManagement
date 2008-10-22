84b6cf35018df6bd23f25c68aa30c0a98573cbd4
Maurits van Rees <maurits@vanrees.org>
2008-10-22 12:54:40 +0000

    Added missing IEmployeesView
    svn path=/Products.eXtremeManagement/trunk/; revision=74197

--- Products/eXtremeManagement/browser/interfaces.py
+++ Products/eXtremeManagement/browser/interfaces.py
@@ -255,3 +255,24 @@ class IXMGlobalState(IBrowserView):
     
     def has_tracker():
         """Is the xm time tracker package available?"""
+
+
+class IEmployeesView(IBrowserView):
+    """A view which displays all employees and their billable percentages
+    per month.
+    """
+
+    def items():
+        """ Returns a list of months each month contains a list of employees
+        and each employee is a dict.
+
+        [{'month':'April',
+          'employees':[
+              {'name':'Mark van Lent',
+               'month_url': 'http://example.com/booking_month?memberid=mark',
+               'percentage': '70 %'},
+              {'name':'Mark van Lent',
+               'month_url': 'http://example.com/booking_month?memberid=mark',
+               'percentage': '70 %'},
+                      ],
+        ]
