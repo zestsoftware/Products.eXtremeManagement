1e75f38a64109309d0416e393e68340b2062e87f
Maurits van Rees <maurits@vanrees.org>
2012-09-12 19:28:42 +0200

    Update setup.py, move files around

--- setup.py
+++ setup.py
@@ -1,31 +1,24 @@
 from setuptools import setup, find_packages
-import os
 
+readme = open('README.txt').read().strip()
+install = open('INSTALL.txt').read().strip()
+authors = open('AUTHORS.txt').read().strip()
+history = open('CHANGES.rst').read().strip()
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-version = read('Products', 'eXtremeManagement', 'version.txt').strip()
-readme = read('Products', 'eXtremeManagement', 'README.txt')
-install = read('Products', 'eXtremeManagement', 'INSTALL.txt')
-authors = read('Products', 'eXtremeManagement', 'AUTHORS.txt')
-history = read('Products', 'eXtremeManagement', 'HISTORY.txt')
-
-long_description = readme + '\n\n' + install + '\n\n' + authors
-long_description += '\n\n' + history
-
+long_description = (
+    readme + '\n\n' + install + '\n\n' + authors + '\n\n' + history)
 
 setup(name='Products.eXtremeManagement',
-      version=version,
+      version='2.1.a3.dev0',
       description="Project administration which supports the eXtreme Programming methodology.",
       long_description=long_description,
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Zope2",
-        "Framework :: Zope3",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "Framework :: Zope3",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
       keywords='',
       author='Zest Software',
       author_email='xm@lists.zestsoftware.nl',
